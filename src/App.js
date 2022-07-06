29fa22b6d9e225bb09c1e9500bd455f1b19b09a8
Per Eskilson <paelsis@hotmail.com>
2022-07-06 17:13:26 +0200

    First version

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment-with-locales-es6'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import './style.css';
+import {getEvents} from './getEvents'
+
+const calendarId='tangokompaniet@gmail.com'
+const apiKey='AIzaSyB0EBiE8xd5ItS59IahMyficWWAanHhMzU' 
+
+const localizer = momentLocalizer(moment)
 
 function App() {
+  const [events, setEvents] = useState([])
+  useEffect(()=>{
+    const timeMin = moment().startOf('day')
+    const timeMax = moment().endOf('month').add(6,'months').add(7, 'days')
+    getEvents(
+      calendarId,
+      apiKey,
+      events => setEvents(events),
+      timeMin.format('YYYY-MM-DD') + 'T00:00:00Z', 
+      timeMax.format('YYYY-MM-DD') + 'T23:59:00Z',
+      'SV'
+    )
+  }, [])
+
+  const handleEvent = e=>alert(e.description)
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 style={{textAlign:'center', color:'orange'}}>Malmö tango kalender</h1>
+        <Calendar
+          localizer={localizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          onSelectEvent={handleEvent}
+          style={{ height: 500 }}
+        />
       </header>
     </div>
   );
---
+++ src/getEvents.js
@@ -0,0 +1,108 @@
+import request from 'superagent'
+import moment from 'moment-with-locales-es6'
+const CULTURE = (language) => language===LANGUAGE_SV?'sv':language===LANGUAGE_ES?'es':'en'
+const CALENDAR_ID_ORIG = 'tb8ckdrm61bdsj6jfm7khob4u5@group.calendar.google.com'
+const API_KEY_ORIG = 'AIzaSyAOuDzSlG24RPBn3OKVAyjW3OK_EJhCUbp'
+//let url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID_ORIG}/events?key=${API_KEY_ORIG}`
+
+// Pers calendar  
+//const CALENDAR_ID = 'e8crjpja77kp13nv2u9q52rkcc@group.calendar.google.com'
+const CALENDAR_ID = 'per.eskilson@gmail.com'
+const API_KEY = 'AIzaSyBMhuxVODuB4qYKmIoSguPYuQuTfct-QIs'
+//const url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}`
+
+//Daniels Tangokompaniet calendar
+const CALENDAR_ID_TK = 'tangokompaniet@gmail.com'
+const API_KEY_TK = 'AIzaSyB0EBiE8xd5ItS59IahMyficWWAanHhMzU' // Per 2
+const LANGUAGE_SV='SV'
+const LANGUAGE_ES='ES'
+
+const findParameter = (s, val) => {
+  const idx = s.indexOf(val)  
+  console.log('findParameter', val, 'idx',  idx)
+  if (idx != -1) {
+    const value = s.slice(idx).match(/(\d+)/)[0]
+    console.log('newString', s.slice(idx), 'value', value)
+    return value
+  } else {
+    return undefined
+  }  
+}  
+
+
+// export means that this function will be available to any module that imports this module
+export function getEvents (calendarId, apiKey, callback, timeMin, timeMax, language) {
+  const url = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${apiKey}&timeMin=${timeMin}&timeMax=${timeMax}&singleEvents=${true}&orderBy=startTime`
+  
+  function cityForEvent (title, location) {
+    if ((title.toLowerCase().indexOf('malmö') !== -1) ||
+        (title.toLowerCase().indexOf('lund') !== -1)) {
+        return undefined 
+    } else {        
+        return location?(location.toLowerCase().indexOf('malmö') !== -1)?'Malmö'
+               :(location.toLowerCase().indexOf('lund') !== -1)?'Lund'
+               :(location.toLowerCase().indexOf('michael') !== -1)?'Lund'
+               :(location.toLowerCase().indexOf('tangokompaniet') !== -1)?'Malmö'
+               :(location.toLowerCase().indexOf('studio') !== -1)?'Malmö'
+               :undefined:undefined          
+    }    
+  }    
+  request
+    .get(url)
+    .end((err, resp) => {
+      if (!err) {
+        // create array to push events into
+        const events = []
+        let previousEndDate=null
+        let dateStyle = null
+        // in practice, this block should be wrapped in a try/catch block, 
+        // because as with any external API, we can't be sure if the data will be what we expect
+        moment.locale(CULTURE(language))
+        let previousWeekNumber = 0
+        let isLightColor = false
+        JSON.parse(resp.text).items.map((it) => {
+          const start = it.start.dateTime?it.start.dateTime:it.start.date
+          const end = it.end.dateTime?it.end.dateTime:it.end.date
+          const description = it.description?it.description:''
+          const location = it.location?it.location.replace(/Tangokompaniet, |, 212 11 |, 224 64|, 223 63|, Sverige|Stiftelsen Michael Hansens Kollegium, /g, ' ').replace('Fredriksbergsgatan','Fredriksbergsg.'):'Plats ej angiven'
+          const sameDate = moment(start).isSame(moment(previousEndDate), 'd') && moment(start).isSame(moment(end), 'd')
+          const isToday = moment().isSame(moment(start), 'day')?true:false
+          const isWeekend = moment(start).isoWeekday() >=6;
+          const calendar = moment(start).calendar()
+          const title = it.summary?it.summary:'No Title'
+          const city = cityForEvent(title, location)
+          const weekNumber = moment(start).isoWeek()
+          const maxPar = Number(findParameter(description, 'MAX_PAR'))
+          const maxInd = Number(findParameter(description, 'MAX_IND'))
+          const maxRegistrants = Number(maxInd?maxInd:maxPar?(maxPar*2):500)
+          const useRegistrationButton = it.description?(it.description.indexOf('MAX_IND')!==-1 || it.description.indexOf('MAX_PAR')!==-1):false
+
+          isLightColor = weekNumber===previousWeekNumber?isLightColor:!isLightColor
+          previousWeekNumber = weekNumber
+
+          let event = {
+            start,
+            end,
+            description,
+            title,
+            location,
+            sameDate,
+            isToday,
+            isWeekend,
+            isLightColor,
+            weekNumber,
+            calendar,
+            city,
+            maxPar,
+            maxInd,
+            maxRegistrants,
+            useRegistrationButton,
+          }  
+          events.push(event)
+          previousEndDate=end
+         
+        })
+        callback(events)
+      } 
+    })
+}
